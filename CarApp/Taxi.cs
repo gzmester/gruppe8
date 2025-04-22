6b56058767c02ee7d566282a204e5e18fc6ec61f
Rasmus <rasmuskaels@hotmail.com>
2025-04-22 12:45:32 +0200

    Update

---
+++ CarApp/Taxi.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using car_app;
+
+
+public class Taxi : Car
+{
+    public double StartPrice { get; }
+    public double PricePerKm { get; } // i kr. per km
+    public double PricePerMinute { get; } // i kr. per minut
+    public bool 
+}
