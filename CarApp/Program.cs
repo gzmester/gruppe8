c89ac5f8b58d18ba601718b7904b904bdbd66aa0
Anders Christensen <ac@skandiko.dk>
2025-04-22 10:01:48 +0200

    car-klasse v3

--- CarApp/Program.cs
+++ CarApp/Program.cs
@@ -7,41 +7,4 @@
             Console.WriteLine("Hello, World!");
         }
     }
-
-    public class Car
-    {
-        public string Brand { get; }
-        public string Model { get; }
-        public string LicensePlate { get; }
-        public bool IsEngineOn { get; private set; }
-        public int Odometer { get; protected set; }
-
-        public Car(string brand, string model, string licensePlate)
-        {
-            Brand = brand;
-            Model = model;
-            LicensePlate = licensePlate;
-            IsEngineOn = false;
-            Odometer = 0;
-        }
-
-        public void StartEngine()
-        {
-            IsEngineOn = true;
-            Console.WriteLine("Engine started.");
-        }
-
-        public void StopEngine()
-        {
-            IsEngineOn = false;
-            Console.WriteLine("Engine stopped.");
-        }
-
-        public virtual void Drive(double distance)
-        {
-            Console.WriteLine($"Driving {distance} km.");
-            Odometer += (int)distance;
-
-        }
-    }
 }
