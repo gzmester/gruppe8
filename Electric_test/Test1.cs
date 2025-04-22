e0d541f05af7dc199b2800d793cfa8b9c27d4d71
Anders Christensen <ac@skandiko.dk>
2025-04-22 10:31:09 +0200

    Test electricEngine

---
+++ Electric_test/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace Electric_test
+{
+    [TestClass]
+    public class ElectricTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ElectricCar electricCar = new ElectricCar("Tesla", "Model S", "CD67890", 1000, 100, 5);
+            electricCar.StartEngine();
+            electricCar.Drive(50);
+            Console.WriteLine($"ElectricCar Odometer: {electricCar.Odometer} km, Battery left: {electricCar.Batterylevel:F2} kWh");
+
+
+
+        }
+    }
+}
