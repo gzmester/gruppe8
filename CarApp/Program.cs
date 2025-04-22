6f16333f7f0d41a883a3a8b5a5189b481a2ae4cf
Anders Christensen <ac@skandiko.dk>
2025-04-22 09:57:48 +0200

    car-klasse

--- CarApp/Program.cs
+++ CarApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace car_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Car
+    {
+        public string Brand { get; }
+        public string Model { get; }
+        public string LicensePlate { get; }
+        public bool IsEngineOn { get; private set; }
+        public int Odometer { get; protected set; }
+
+        public Car(string brand, string model, string licensePlate)
+        {
+            Brand = brand;
+            Model = model;
+            LicensePlate = licensePlate;
+            IsEngineOn = false;
+            Odometer = 0;
+        }
+
+        public void StartEngine()
+        {
+            IsEngineOn = true;
+            Console.WriteLine("Engine started.");
+        }
+
+        public void StopEngine()
+        {
+            IsEngineOn = false;
+            Console.WriteLine("Engine stopped.");
+        }
+
+    }
+}
