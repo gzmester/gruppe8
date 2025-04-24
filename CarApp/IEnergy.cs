b79e5a03f1631aa6ee22b872086fa8d0b22946ea
Alexander Skøtt Leismann <asle71355@edu.ucl.dk>
2025-04-24 10:26:17 +0200

    IEnergy

---
+++ CarApp/IEnergy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using car_app;
+namespace car_app
+{
+    internal interface IEnergy
+    {
+        double EnergyLevel { get; }
+        double MaxEnergy {  get; }
+
+        void Refill(double amount);
+
+        void UseEnergy(double km);
+
+    }   
+}
